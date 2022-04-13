44129403799a5fc3b3e78362ddd143909019e0db
Omer <omeramasib999@gmail.com>
2022-04-13 23:25:50 +0300

    edit in employment application screen

--- lib/utils/widgets/employment_applications.dart
+++ lib/utils/widgets/employment_applications.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class EmploymentApplications extends StatelessWidget {
-  const EmploymentApplications({ Key? key }) : super(key: key);
+  const EmploymentApplications({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +10,7 @@ class EmploymentApplications extends StatelessWidget {
     var height = size.height;
     var width = size.width;
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         title: Text(
           "طلبات التوظيف",
           style: GoogleFonts.cairo(
@@ -27,13 +28,131 @@ class EmploymentApplications extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 width: width,
-                height: height *0.80,
-                decoration:BoxDecoration(
+                height: height * 0.90,
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(5),
-                  color: Colors.red,
+                  // color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
                 ),
                 child: Card(
-                  
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Container(
+                          width: width / 1.10,
+                          height: height / 12,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius: BorderRadius.circular(5)
+                              ),
+                          child: Center(
+                            child: Text(
+                              "الإنضمام لفريق العمل",
+                              style: GoogleFonts.cairo(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "مرحبا بك في صفحة التوظيف ان كنت مسوق عقاري وتريد الإنضمام الينا يرجى ملئ بياناتك وسنتواصل معك",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.cairo(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "الإسم الكامل",
+                              style: GoogleFonts.cairo(
+                                  fontSize: 18, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                      ),
+                        SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: width / 1.10,
+                              height: 50,
+                              decoration: BoxDecoration(
+                              color: Colors.deepPurple[100],
+                              borderRadius: BorderRadius.circular(3)
+                              ),
+                              child: TextFormField(
+                                keyboardType:  TextInputType.text,
+                                style: TextStyle(color: Colors.black54),
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Color(0xffe8ae30), width: 2.0),
+                                    borderRadius: BorderRadius.circular(3),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                             Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "البريد الإلكتروني",
+                              style: GoogleFonts.cairo(
+                                  fontSize: 18, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                      ),
+                        SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              width: width / 1.10,
+                              height: 50,
+                              decoration: BoxDecoration(
+                              color: Colors.deepPurple[100],
+                              borderRadius: BorderRadius.circular(3)
+                              ),
+                              child: TextFormField(
+                                keyboardType:  TextInputType.emailAddress,
+                                style: TextStyle(color: Colors.black54),
+                                decoration: InputDecoration(
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Color(0xffe8ae30), width: 2.0),
+                                    borderRadius: BorderRadius.circular(3),
+                                  ),
+                                ),
+                              ),
+                            ),
+                    ],
+                  ),
                 ),
               ),
             )
